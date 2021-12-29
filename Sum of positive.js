afb7ef0c1ac14763af9103b3f401a300eb66e20b
1t-n00b <mummintrol@gmail.com>
2021-12-29 23:03:58 +0200

    solution task

--- Sum of positive.js
+++ Sum of positive.js
@@ -1,3 +1,8 @@
 function positiveSum(arr) {
-
+    let x=0;
+    for (let i = 0; i <arr.length ; i++) {
+        if (arr[i]>0) x=x+arr[i]
+    }
+    return x
 }
+console.log(positiveSum([5,-1,-2,3,-1,5]));
