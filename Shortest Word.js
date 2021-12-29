c0397fe29b55712ec8b9e637732628c208ab60e3
1t-n00b <mummintrol@gmail.com>
2021-12-29 15:56:23 +0200

    solution task

--- Shortest Word.js
+++ Shortest Word.js
@@ -1,3 +1,7 @@
 function findShort(s){
+    return  s.split(" ").sort(((a, b) => a.length - b.length))[0].length;
+
 
 }
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
