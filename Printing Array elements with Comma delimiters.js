95a6e02f0f4a5cdb3010cc14be073d5caf9cf021
1t-n00b <mummintrol@gmail.com>
2021-12-31 11:16:53 +0200

    solution task

---
+++ Printing Array elements with Comma delimiters.js
@@ -0,0 +1,11 @@
+function printArray(array){
+    //show me the code!
+    // let str ="";
+    // for (let i = 0; i <array.length ; i++) {
+    //     str +=array[i] + ",";
+    // }
+    // return str.substring(0,str.length-1)
+    return array.toString();
+}
+
+console.log(printArray(["a","b","rthvcb"]))
