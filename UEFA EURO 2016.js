3f9db611a51aaa2b601a3a26a73f9d9cb4c869ac
1t-n00b <mummintrol@gmail.com>
2022-01-01 17:05:58 +0200

    solution task

---
+++ UEFA EURO 2016.js
@@ -0,0 +1,10 @@
+function uefaEuro2016(teams, scores){
+    // your code...
+    if (scores[0]>scores[1])
+        return  "At match "   +teams[0]+ " - " +teams[1]+", " +teams[0]+ " won!";
+    else if (scores[1]>scores[0])
+        return  "At match "   +teams[0]+ " - " +teams[1]+", " +teams[1]+ " won!";
+    else return "At match "   +teams[0]+ " - " +teams[1]+","+"teams played draw."
+}
+
+console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0]));
