a8148e01c693b869e40bad689d6bcc71bfa30eed
1t-n00b <mummintrol@gmail.com>
2022-02-02 20:12:41 +0200

    solution task

---
+++ Coding Meetup #2 - Higher-Order Functions Series - Greet developers.js
@@ -0,0 +1,14 @@
+function greetDevelopers(list) {
+
+
+       list.map( dev=> dev.greeting =  `Hi ${dev.firstName}, what do you like the most about ${dev.language}?`)
+    return list
+   
+}
+var list1 = [
+    { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+    { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
+];
+ greetDevelopers(list1)
+console.log(list1)
